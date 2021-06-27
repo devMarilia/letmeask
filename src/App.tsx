63b067f4328253a2d1a1e2594662901486222b09
devMarilia <devMarilia.frontend@gmail.com>
2021-06-27 18:37:16 -0300

    :package: nova funcionalidade

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { BrowserRouter, Route, Switch} from 'react-router-dom'
+
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom';
-import { AuthContexProvider } from './contexts/AuthContext'
 import { Room } from './pages/Room';
+import { AdminRoom } from './pages/AdminRoom';
+
+import { AuthContexProvider } from './contexts/AuthContext'
 
 function App() {
   return (
@@ -12,6 +15,8 @@ function App() {
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/new" exact component={NewRoom}/> 
         <Route path="/rooms/:id" component={Room}/> 
+
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch>
       </AuthContexProvider>   
    </BrowserRouter>
