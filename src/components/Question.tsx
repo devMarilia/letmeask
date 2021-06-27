f59a6679c7cd9f39616137707090ea1336a13345
devMarilia <devMarilia.frontend@gmail.com>
2021-06-27 19:55:45 -0300

    :package: nova funcionalidade

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from 'react';
-
+import cx from 'classnames';
 import '../styles/question.scss'
 
 type QuestionProps = {
@@ -9,26 +9,36 @@ type QuestionProps = {
       avatar: string;
     };
     children?: ReactNode;
+    isAnswered?: boolean;
+    isHighlighted?: boolean;
   }
   
   export function Question({
     content,
     author,
+    isAnswered = false,
+    isHighlighted = false,
     children,
   }: QuestionProps) {
     return (
-      <div className="question">
-        <p>{content}</p>
-        <footer>
-          <div className="user-info">
-            <img src={author.avatar} alt={author.name} />
-            <span>{author.name}</span>
-          </div>
-          <div>
-            {children}
-          </div>
-        </footer>
-      </div>
+      <div 
+      className={cx(
+        'question', 
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered },
+      )}
+    >
+      <p>{content}</p>
+      <footer>
+        <div className="user-info">
+          <img src={author.avatar} alt={author.name} />
+          <span>{author.name}</span>
+        </div>
+        <div>
+          {children}
+        </div>
+      </footer>
+    </div>
     );
   }
   
